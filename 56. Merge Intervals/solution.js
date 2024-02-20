afb4e6d2623fd8600f93d5bed5f1a3b8dc516f07
unknown <simionescuionut1@gmail.com>
2024-02-20 17:38:36 +0000

    Solution

--- 56. Merge Intervals/solution.js
+++ 56. Merge Intervals/solution.js
@@ -2,23 +2,27 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-var merge = function(intervals) {
-    // So as a lot of things you want to start by sorting ascendingly, based on the start value
-    intervals.sort((a, b) => a[0] - b[0])
-    // then create a new array for the merged result
-    const mergedIntervals = [];
-    // if you have no overlap or there's an empty array then you just append it to the merged result
-    // else resolve the conflict by merging the two elements
-        // for the example of [1, 3] and [2, 6] the it should be something like "if i[1]<j[0]" then append, else 
-        // pop i[1] append max (i, j) 
-    for (const interval of intervals) {
-        if (merged.length === 0 )
-    }
-};
-
-// So as a lot of things you want to start by sorting ascendingly
+// So as a lot of things you want to start by sorting ascendingly, based on the start value
 // then create a new array for the merged result
 // if you have no overlap or there's an empty array then you just append it to the merged result
 // else resolve the conflict by merging the two elements
-// for the example of [1, 3] and [2, 6] the it should be something like "if i<j" then append, else
+// for the example of [1, 3] and [2, 6] the it should be something like "if i[1]<j[0]" then append, else
 // pop i[1] append max (i, j)
+
+var merge = function (intervals) {
+  if (intervals.length === 1) return intervals;
+  intervals.sort((a, b) => a[0] - b[0]);
+  let curr = intervals[0];
+  const mergedIntervals = [curr];
+  for (let i = 1; i < intervals.length; i += 1) {
+    const currRight = curr[1];
+    const [nextLeft, nextRight] = intervals[i];
+    if (currRight >= nextLeft) {
+      curr[1] = Math.max(currRight, nextRight);
+    } else {
+      mergedIntervals.push(intervals[i]);
+      curr = intervals[i];
+    }
+  }
+  return mergedIntervals;
+};
